58c733a45ae6a6e23b7cf8f7636f2160fa567b61
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2022-12-27 17:15:31 +0100

    Add finaly photo of the property using ActiveStorage

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,13 @@
-class Property < ApplicationRecord
+#class Property < ApplicationRecord
+class Property < ActiveRecord::Base
+  has_one_attached :photo
   belongs_to :account
+
+  def banner
+    self.photo.variant(resize: '100x75!').processed
+  end
+
+  def thumbnail
+    self.photo.variant(resize: '100x100!').processed
+  end
 end
