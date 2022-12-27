d11780e63340e378054e130d9327ea8ed9e568a3
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2022-12-27 18:19:16 +0100

    dashboard editing

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,13 @@
 #class Property < ApplicationRecord
-class Property < ActiveRecord::Base
+class Property < ActiveRecord::Base #< ApplicationRecord
   has_one_attached :photo
   belongs_to :account
+
+  scope :latest, -> { order created_at:  :desc}
+
+  def public_page
+    self.photo.variant(resize: '300x200!').processed
+  end
 
   def banner
     self.photo.variant(resize: '100x75!').processed
