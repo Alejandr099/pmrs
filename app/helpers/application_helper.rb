43c7155610490db9bf0eba3ddfba7c85d9b1c519
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2022-12-29 17:50:03 +0100

    Modified app controller, add method to app helper, layouts and images fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def profile_avatar account
+    thumb = account.avatar.present? ? account.avatar : "default-profile.jpg"
+    image_tag thumb, class: "profile-pic img-circle", style: "border-radius:90px;, width:150px; height:150px;"
+  end
 end
