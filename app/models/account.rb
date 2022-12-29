0a2aed0eacc57a8b3dbb55b94a17f930239edcb5
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2022-12-30 00:03:26 +0100

    Modified account.rb and _list.html.erb

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,13 @@
 class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :trackable, :confirmable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :trackable,
+         :confirmable
 
   has_many :properties
   has_one_attached :avatar
