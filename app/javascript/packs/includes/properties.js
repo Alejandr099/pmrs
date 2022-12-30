4df67bd1619188f12bafd61cb59c8dcad7d44475
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2022-12-30 14:25:04 +0100

    Rebuild button Show more on property show page

--- app/javascript/packs/includes/properties.js
+++ app/javascript/packs/includes/properties.js
@@ -1,7 +1,12 @@
 $(function (){
     $('#toggleDetails').on("click", function() {
         var details = $('#prop-details');
-
         details.toggleClass("open");
+
+        if(details.hasClass("open")) {
+            $(this).text("Show less");
+        } else {
+            $(this).text("Show more");
+        }
     })
 });
