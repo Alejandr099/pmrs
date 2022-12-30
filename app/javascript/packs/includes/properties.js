228af0768b9d0168d4916150f757f5e967a7223e
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2022-12-30 20:45:03 +0100

    Finally add popup messages by putting <script> tags for bootstrap at bottom application.html.erb . It's working good.

--- app/javascript/packs/includes/properties.js
+++ app/javascript/packs/includes/properties.js
@@ -1,17 +1,17 @@
 // toggle show description property button
-$(function (){
-    $('#toggleDetails').on("click", function() {
+$(function() {
+    $('#toggleDetails').on("click", function () {
         var details = $('#prop-details');
         details.toggleClass("open");
 
-        if(details.hasClass("open")) {
+        if (details.hasClass("open")) {
             $(this).text("Show less");
         } else {
             $(this).text("Show more");
         }
     });
-
 // Message popup script
+
     $('#contact-modal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget);
         var modal = $(this);
