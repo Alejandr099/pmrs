e6bc9ba00a5665c1be41d80ecdbf1d87a9c50f5f
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2023-01-01 16:30:55 +0100

    Style edit account page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
+      @show_sidebar = true
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :url, :avatar, :company])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar])
     end
