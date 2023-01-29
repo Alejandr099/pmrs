8169864984053cf73ac631353b698e3bdafa194e
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2023-01-29 14:09:13 +0100

    Add methods to Res controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,22 @@
 class ReservationsController < ApplicationController
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = current_account.reservation.build(reservation_params)
+    if @reservation.save
+      redirect_to @reservation
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :property_id)
+  end
+
 end
