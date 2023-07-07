a752450d615ff6c417cad7195b5eb7ec1c34a994
aleksanderwozniczka <aleksander.wozniczka99@gmail.com>
2023-07-07 18:47:02 +0200

    Fix the executing background jobs

--- app/jobs/delete_expired_reservations_job.rb
+++ app/jobs/delete_expired_reservations_job.rb
@@ -1,8 +1,9 @@
 class DeleteExpiredReservationJob < ApplicationJob
-  include Sidekiq::Job
+  #include Sidekiq::Job
   queue_as :default
 
   def perform(*)
     Reservation.where("end_date < ?", Time.now).destroy_all
+    
   end
 end
